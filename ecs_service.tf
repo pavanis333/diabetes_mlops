9ddc5674062ca2c4ae3215485231bdc1ea8b3fec
pavanis333 <63490819+pavanis333@users.noreply.github.com>
2021-05-23 00:19:09 +0530

    Update ecs_service.tf

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,9 +1,17 @@
 resource "aws_ecs_service" "deeplearning_service" {
-    name            = var.service_name
+        depends_on      = [aws_lb_listener.cluster_lb_listener, aws_iam_role_policy_attachment.ecs_task_execution_role]
+        name            = var.service_name
   	cluster         = aws_ecs_cluster.deeplearning_cluster.id
-  	task_definition = aws_ecs_task_definition.deeplearning_task_definition.family
+  	task_definition = aws_ecs_task_definition.deeplearning_task_definition.arn
   	desired_count   = 2
+	launch_type     = "FARGATE"
 
+        network_configuration {
+        security_groups  = [aws_security_group.sg_lb.id]
+        subnets          = data.aws_subnet_ids.default.ids
+        assign_public_ip = true
+        }
+	
   	load_balancer {
     	target_group_arn  = aws_lb_target_group.cluster_target_group.arn
     	container_port    = 80
