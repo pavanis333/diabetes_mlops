0f9fa858dbd90534cdcb5c02a9bf62ef13ed9300
pavanis333 <63490819+pavanis333@users.noreply.github.com>
2021-05-23 00:29:49 +0530

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -7,13 +7,6 @@ resource "aws_lb" "cluster_lb" {
   subnets            = data.aws_subnet_ids.default.ids
 }
 
-resource "aws_lb_target_group" "cluster_target_group" {
-  name = "cluster-target-group"
-  port = 80
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_lb_listener" "cluster_lb_listener"{
   load_balancer_arn = aws_lb.cluster_lb.arn
   port = 80
@@ -23,4 +16,12 @@ resource "aws_lb_listener" "cluster_lb_listener"{
     target_group_arn = aws_lb_target_group.cluster_target_group.arn
     type = "forward"
   }
+}
+
+resource "aws_lb_target_group" "cluster_target_group" {
+  name = "cluster-target-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+  target_type = "ip"
 }
