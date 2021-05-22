e05d11cacaaec441b219f31986b0faa6073a536d
pavanis333 <63490819+pavanis333@users.noreply.github.com>
2021-05-22 23:06:56 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker
+sudo systemctl start docker
