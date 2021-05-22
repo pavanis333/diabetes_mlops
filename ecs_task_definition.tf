bf3630da81a367237b2e75bc288468e6b691046d
pavanis333 <63490819+pavanis333@users.noreply.github.com>
2021-05-22 23:35:34 +0530

    Update ecs_task_definition.tf

--- ecs_task_definition.tf
+++ ecs_task_definition.tf
@@ -2,11 +2,13 @@ resource "aws_ecs_task_definition" "deeplearning_task_definition" {
   depends_on = [aws_ecs_cluster.deeplearning_cluster]
   family = "deeplearning_task_definition"
   requires_compatibilities = ["FARGATE"]
+  cpu = 1024
+  memory = 2048
   container_definitions = <<TASK_DEFINITION
   [
   {
-    "cpu" : 1024,
-    "memory" : 2048,
+    "cpu" : 512,
+    "memory" : 1024,
     "name": "${var.container_name}",
     "image": "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.image_name}:latest",
     "essential": true,
